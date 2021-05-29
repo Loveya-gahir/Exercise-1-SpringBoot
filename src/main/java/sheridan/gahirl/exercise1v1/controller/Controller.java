9739a3e2b7415f446167e83c3441f1edadb22f32
Administrator <gahirl@sheridancollege.ca>
2021-05-29 19:49:18 -0400

    Committing Unversioned Files

---
+++ src/main/java/sheridan/gahirl/exercise1v1/controller/Controller.java
@@ -0,0 +1,4 @@
+package sheridan.gahirl.exercise1v1.controller;
+
+public class Controller {
+}
---
+++ src/test/java/sheridan/gahirl/exercise1v1/Exercise1v1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.gahirl.exercise1v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise1v1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
