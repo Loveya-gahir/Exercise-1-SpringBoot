eab2c30fc2e8266915ceb0874bce81b0dae03b62
Administrator <gahirl@sheridancollege.ca>
2021-05-30 19:28:21 -0400

    Added mapping, request param and css.

--- src/main/java/sheridan/gahirl/exercise1v1/controller/Controller.java
+++ src/main/java/sheridan/gahirl/exercise1v1/controller/Controller.java
@@ -1,4 +1,29 @@
 package sheridan.gahirl.exercise1v1.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@org.springframework.stereotype.Controller
+
 public class Controller {
+
+
+    @GetMapping("/Input")
+    public String input() {
+
+
+        return "Input";
+    }
+
+    @GetMapping("/Output")
+    public String output(@RequestParam String name,
+                         Model model) {
+
+
+        model.addAttribute("name", name);
+        return "Output";
+    }
 }
+
+
